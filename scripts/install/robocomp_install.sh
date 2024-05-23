c6d45af0d40f91e793734020d1441026ca74af06
Alberto Tudela <ajtudela@gmail.com>
2024-05-23 21:09:50 +0200

    Update CI workflows
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- scripts/install/robocomp_install.sh
+++ scripts/install/robocomp_install.sh
@@ -1,9 +1,9 @@
 set -e
 cat /etc/issue || true
 branch="${branch:-development}"
-source <(curl -sL https://raw.githubusercontent.com/robocomp/robocomp/$branch/scripts/install/resources/robocomp_prerequisites_install.sh)
+source <(curl -sL https://raw.githubusercontent.com/grupo-avispa/robocomp/$branch/scripts/install/resources/robocomp_prerequisites_install.sh)
 
-git clone -b $branch https://github.com/robocomp/robocomp.git
+git clone -b $branch https://github.com/grupo-avispa/robocomp.git
 sudo ln -s ~ /home/robocomp
 echo "export ROBOCOMP=~/robocomp" >> ~/.bashrc
 echo "export PATH=$PATH:/opt/robocomp/bin" >> ~/.bashrc
